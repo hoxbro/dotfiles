4cac03589fbcc6ffe23c2adfddfac80d24f226a9
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-01-26 15:56:41 +0100

    add treesj

--- .config/nvim/lua/personal/lazy/trial.lua
+++ .config/nvim/lua/personal/lazy/trial.lua
@@ -1,1 +1,15 @@
-return {}
+return {
+    {
+        "Wansmer/treesj",
+        keys = {
+            {
+                "<leader>cs",
+                function() require("treesj").toggle() end,
+                desc = "Treesitter split and join",
+                mode = { "n", "x" },
+            },
+        },
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        opts = { use_default_keymaps = false },
+    },
+}
