b60a75079ff85f94bcfe685a3d5f193721474d05
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-04-07 14:10:26 +0200

    chore: Update lazy

--- .config/nvim/lua/personal/lazy/test.lua
+++ .config/nvim/lua/personal/lazy/test.lua
@@ -3,7 +3,7 @@ return {
         "nvim-neotest/neotest",
         dependencies = {
             "nvim-lua/plenary.nvim",
-            "antoinemadec/FixCursorHold.nvim",
+            "antoinemadec/fixcursorhold.nvim",
             "nvim-treesitter/nvim-treesitter",
             "nvim-neotest/nvim-nio",
         },
