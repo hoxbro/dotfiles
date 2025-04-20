c562f27899004af81c30a7799808e64a09df4669
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-04-20 16:37:37 +0200

    Update neovim to 0.11.0

--- .config/nvim/lua/personal/lazy/telescope.lua
+++ .config/nvim/lua/personal/lazy/telescope.lua
@@ -31,7 +31,7 @@ end
 
 return {
     "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
+    branch = "master",
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope-ui-select.nvim",
