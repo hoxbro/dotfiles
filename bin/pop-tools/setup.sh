fc417eea0e70c7a204bbb696e107ac724be86f24
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-05-23 14:47:44 +0200

    kiss setup

--- bin/pop-tools/setup.sh
+++ bin/pop-tools/setup.sh
@@ -25,43 +25,19 @@ meshify) FUNCTIONS+=(setup_wol) ;;
 # virtm) ;;
 esac
 
-
 # The machine
-spinner() {
-    spinner_chars="|/-\\"
-    delay=0.1
-    local pid=$1
-    local i=0
-    while kill -0 "$pid" 2>/dev/null; do
-        if ps -o stat= -p "$pid" | grep -q T; then
-            printf "\rI"
-        else
-            i=$(((i + 1) % 4))
-            printf "\r%s" "${spinner_chars:$i:1}"
-        fi
-        sleep $delay
-    done
-}
-
-echo -e "\e[?25l"
 echo "Running functions in setup:"
 TOTAL=${#FUNCTIONS[@]}
 COUNT=1
 for FUNCTION in "${FUNCTIONS[@]}"; do
-    printf "\r   %s/%s %s" "$COUNT" "$TOTAL" "${FUNCTION//_/ }"
+    echo -n "$COUNT"/"$TOTAL" "${FUNCTION//_/ }"
     LOGNAME=~/Downloads/setup"$COUNT"_${FUNCTION/install_/}.log
     SECONDS=0
-    (set -euxo pipefail && "$FUNCTION") &>"$LOGNAME" &
-    cmd_pid=$!
-    spinner $cmd_pid
-    wait $cmd_pid
-    exitcode=$?
-    printf "\r   %s/%s %s" "$COUNT" "$TOTAL" "${FUNCTION//_/ }"
-    if ((exitcode > 0)); then printf " !!! failed !!!"; fi
-    printf " (%d min and %d sec)\n" $(( (SECONDS / 60) % 60 )) $(( SECONDS % 60 ))
+    (set -euxo pipefail && "$FUNCTION") &>"$LOGNAME"
+    if (($? > 0)); then echo -n " !!! failed !!!"; fi
+    echo " ($((("$SECONDS" / 60) % 60)) min and $(("$SECONDS" % 60)) sec)"
     COUNT=$((COUNT + 1))
 done
-echo -e "\e[?25h"
 
 if [ -f /var/run/reboot-required ]; then
     echo 'reboot required'
