8a96068475c5a3199f2ed90dfc4b9e9a0e80962f
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-05-24 19:48:17 +0200

    make leader+c just c in nvim mode

--- .config/nvim/lua/personal/lazy/treesitter.lua
+++ .config/nvim/lua/personal/lazy/treesitter.lua
@@ -40,7 +40,7 @@ return {
         "wansmer/treesj",
         keys = {
             {
-                "<leader>cs",
+                "cs",
                 function() require("treesj").toggle() end,
                 desc = "Treesitter split and join",
                 mode = { "n", "x" },
