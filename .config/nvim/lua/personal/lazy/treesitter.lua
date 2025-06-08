4afe72acdd920f1721ad447e4946c3b9811a6686
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-06-08 21:48:01 +0200

    Move treesitter to main branch

--- .config/nvim/lua/personal/lazy/treesitter.lua
+++ .config/nvim/lua/personal/lazy/treesitter.lua
@@ -3,12 +3,12 @@ return {
         "nvim-treesitter/nvim-treesitter",
         event = { "BufReadPost", "BufWritePost", "BufNewFile", "VeryLazy" },
         cmd = { "TSUpdateSync", "TSUpdate" },
+        branch = "main",
         dependencies = {
-            "nvim-treesitter/nvim-treesitter-textobjects",
+            { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
             { "nvim-treesitter/nvim-treesitter-context", opts = { max_lines = 10 } },
         },
         build = ":TSUpdate",
-        main = "nvim-treesitter.configs",
         opts = {
             ensure_installed = {
                 "lua",
@@ -30,11 +30,33 @@ return {
                 "dockerfile",
                 "nginx",
             },
-
-            auto_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
         },
+        config = function(_, opts)
+            require("nvim-treesitter").setup()
+
+            -- Install parsers
+            local installed = require("nvim-treesitter.config").get_installed()
+            local to_install = vim.iter(opts.ensure_installed)
+                :filter(function(parser) return not vim.tbl_contains(installed, parser) end)
+                :totable()
+            if #to_install > 0 then require("nvim-treesitter").install(to_install) end
+
+            -- Autostart
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function(details)
+                    local bufnr = details.buf
+                    -- Highlight
+                    if not pcall(vim.treesitter.start, bufnr) then return end
+                    -- Indentation
+                    vim.bo[bufnr].indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+                    -- Folds
+                    vim.bo[bufnr].syntax = "on"
+                    vim.wo.foldlevel = 99
+                    vim.wo.foldmethod = "expr"
+                    vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                end,
+            })
+        end,
     },
     {
         "wansmer/treesj",
