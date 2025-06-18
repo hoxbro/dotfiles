759a5ffd068cc0512510a73ee2c70aa13e8e97a7
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-06-18 09:31:50 +0200

    Call from nvim setup from setup

--- bin/pop-tools/setup/nvim.sh
+++ bin/pop-tools/setup/nvim.sh
@@ -2,12 +2,8 @@
 
 set -euox pipefail
 
-# NOTE: This should in theory work, but does not...
-# Treesitter only installs some of the dependencies
-# Same with Mason, this could be explained by lack of
-# correct Python version in the path. Adding Conda work
-# (sometimes). Solution to both is to open nvim up after
-# the script has finished.
+zsh -i -c $'
+sleep 1
 nvim --headless "+Lazy! restore" +qa
 nvim --headless "+MasonLockRestore" +qa
-# TODO: blink.cmp binary download too
+'
