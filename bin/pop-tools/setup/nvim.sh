1fb6d1f93c98071bc92504f928059fb2681915a8
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-06-19 13:23:03 +0200

    add blink binary download

--- bin/pop-tools/setup/nvim.sh
+++ bin/pop-tools/setup/nvim.sh
@@ -5,5 +5,27 @@ set -euox pipefail
 ZSH_EAGER=1 zsh -i -c $'
 nvim --headless "+Lazy! restore" +qa
 nvim --headless "+MasonUpdate" +qa
-nvim --headless "+MasonLockRestore" +qa
-'
+nvim --headless "+MasonLockRestore" +qa'
+
+# Blink download
+BLINK_PATH=~/.local/share/nvim/lazy/blink.cmp/target/release
+mkdir -p "$BLINK_PATH"
+cd "$BLINK_PATH"
+
+VERSION=$(git describe --tags --abbrev)
+case "$(uname)" in
+Linux) EXT=".so" ;;
+Darwin) EXT=".dylib" ;;
+*) EXT=".dll" ;;
+esac
+ASSET="$(rustc --print host-tuple)"
+NAME="libblink_cmp_fuzzy${EXT}"
+
+curl -sLo "${NAME}" "https://github.com/Saghen/blink.cmp/releases/download/${VERSION}/${ASSET}${EXT}"
+curl -sLo "${NAME}.sha256" "https://github.com/Saghen/blink.cmp/releases/download/${VERSION}/${ASSET}${EXT}.sha256"
+
+mkdir -p "${ASSET}"
+cp "${NAME}" "${ASSET}/${ASSET}${EXT}"
+sha256sum -c "${NAME}.sha256"
+rm "${ASSET}/${ASSET}${EXT}"
+echo "${VERSION}" >version
