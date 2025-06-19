e6ae872d427c81196bc45d585277f79f08e17c6a
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-06-19 22:21:57 +0200

    Add BlinkDownload

--- .config/nvim/lua/personal/lazy/cmp.lua
+++ .config/nvim/lua/personal/lazy/cmp.lua
@@ -1,7 +1,22 @@
+local download = function()
+    local download = require("blink.cmp.fuzzy.download")
+    local files = require("blink.cmp.fuzzy.download.files")
+
+    -- Avoid EEXIST error
+    vim.fn.mkdir(files.lib_folder, "p")
+
+    print("Downloading pre-built binary\n")
+    download.ensure_downloaded(function()
+        print("Finished downloading pre-built binary\n")
+        vim.defer_fn(function() vim.cmd("qa!") end, 10)
+    end)
+end
+
 return {
     {
         "saghen/blink.cmp",
         event = "VeryLazy",
+        cmd = "BlinkDownload",
         version = "1.*",
         opts = {
             completion = {
@@ -11,6 +26,7 @@ return {
             sources = {
                 default = { "lazydev", "lsp", "path", "buffer", "snippets", "copilot" },
                 providers = {
+                    -- move copilot into own file see lazyvim
                     copilot = { name = "copilot", module = "blink-copilot", score_offset = 100, async = true },
                     lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", score_offset = 100 },
                 },
@@ -19,5 +35,9 @@ return {
             signature = { enabled = true },
             keymap = { ["<C-e>"] = { "hide", "show" } },
         },
+        config = function(_, opts)
+            require("blink-cmp").setup(opts)
+            vim.api.nvim_create_user_command("BlinkDownload", download, { desc = "Download binary" })
+        end,
     },
 }
