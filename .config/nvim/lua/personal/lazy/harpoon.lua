af6dd643b7a3965b10d03e49a57265690319c177
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-08-25 15:15:25 +0200

    update harpoon to not be sensitive to reloading

--- .config/nvim/lua/personal/lazy/harpoon.lua
+++ .config/nvim/lua/personal/lazy/harpoon.lua
@@ -4,14 +4,14 @@ local function get_git_branch()
     return git_branch[1]
 end
 
-local harpoon -- To be able to lazy load it
+_G.__harpoon = _G.__harpoon or nil -- To be able to lazy load it
 
 local function harpoon_list()
     local branch = get_git_branch()
     if branch == nil then
-        return harpoon:list()
+        return _G.__harpoon:list()
     else
-        return harpoon:list(branch)
+        return _G.__harpoon:list(branch)
     end
 end
 
@@ -19,10 +19,10 @@ return {
     "theprimeagen/harpoon",
     dependencies = { "nvim-lua/plenary.nvim" },
     branch = "harpoon2",
-    config = function() harpoon = require("harpoon"):setup() end,
+    config = function() _G.__harpoon = require("harpoon"):setup() end,
     keys = {
         { "<leader>a", function() harpoon_list():add() end, desc = "Add to Harpoon" },
-        { "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon_list()) end, desc = "Edit Harpoon List" },
+        { "<leader>e", function() _G.__harpoon.ui:toggle_quick_menu(harpoon_list()) end, desc = "Edit Harpoon List" },
         { "<leader>h", function() harpoon_list():select(1) end, desc = "First Item on Harpoon List" },
         { "<leader>j", function() harpoon_list():select(2) end, desc = "Second Item on Harpoon List" },
         { "<leader>k", function() harpoon_list():select(3) end, desc = "Third Item on Harpoon List" },
