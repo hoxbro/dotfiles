87112d7d97fa86ecc8c0fd3fee3d0de7084e1bc6
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-11-01 13:50:06 +0000

    Update

--- bin/pop-tools/setup/yay.sh
+++ bin/pop-tools/setup/yay.sh
@@ -4,7 +4,7 @@ PACKAGES=(
     # Terminal
     less rsync lsof zsh rar stow wl-clipboard man-db zip unzip
     ffmpeg parallel trash-cli btop yt-dlp downgrade ufw pass
-    asciinema asciinema-agg
+    asciinema asciinema-agg-bin
 
     # Desktop Environment
     noto-fonts ttf-ubuntu-mono-nerd
