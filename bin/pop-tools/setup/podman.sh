8d0bdb65e4761a1ba1eb9cd9fc492efcaee6ba60
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-11-19 09:49:35 +0100

    Add nvidia-container-toolkit to podman

--- bin/pop-tools/setup/podman.sh
+++ bin/pop-tools/setup/podman.sh
@@ -2,6 +2,12 @@
 
 set -euox pipefail
 sudo -v || exit 1
-yay -S podman podman-desktop podman-compose crun --noconfirm --needed
 
+pkgs=(podman podman-desktop podman-compose crun)
+
+case $(cat /etc/hostname) in
+meshify) pkgs+=(nvidia-container-toolkit) ;;
+esac
+
+yay -S "${pkgs[@]}" --needed --noconfirm
 mkdir -p ~/.config/containers && printf '[engine]\ncompose_warning_logs = false\n' >~/.config/containers/containers.conf
