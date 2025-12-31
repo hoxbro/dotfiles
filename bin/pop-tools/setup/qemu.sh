322bc654620fe06520d3174bf76cf53a62f92b37
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-12-31 11:45:37 +0100

    update qemu setup

--- bin/pop-tools/setup/qemu.sh
+++ bin/pop-tools/setup/qemu.sh
@@ -8,16 +8,12 @@ if (($(grep -E -c '(vmx|svm)' /proc/cpuinfo) > 0)); then
     yay -S qemu-desktop virt-manager swtpm spice-vdagent dnsmasq --needed --noconfirm
 
     sudo usermod -a -G libvirt "$(whoami)"
-    newgrp libvirt
 
     sudo sed -i 's/^#\(unix_sock_group = "libvirt"\)/\1/' /etc/libvirt/libvirtd.conf
     sudo sed -i 's/^#\(unix_sock_rw_perms = "0770"\)/\1/' /etc/libvirt/libvirtd.conf
 
     sudo systemctl enable --now libvirtd
-    sudo systemctl start libvirtd
-
     sudo virsh net-autostart default
-    # Note seem to need a restart to work
 else
     echo "Missing virtualization extension!!!"
     exit 10
