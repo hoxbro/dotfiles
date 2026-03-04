d5a08ca119f11b24c289b0db05e487877275cebf
Simon Høxbro Hansen <hoxbro@protonmail.com>
2026-03-04 16:52:31 +0100

    Remove sql.lua

--- .config/nvim/lua/personal/lazy/treesitter.lua
+++ .config/nvim/lua/personal/lazy/treesitter.lua
@@ -19,7 +19,7 @@ return {
             require("nvim-treesitter").update()
         end,
         opts_extend = { "install" },
-        opts = { install = { "json", "toml", "xml", "yaml" } },
+        opts = { install = { "json", "toml", "xml", "yaml", "sql" } },
         config = function(_, opts)
             require("nvim-treesitter").setup()
 
