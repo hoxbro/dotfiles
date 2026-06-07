41b34af90ece08998441d53194e0b038b428d992
Simon Høxbro Hansen <hoxbro@protonmail.com>
2026-06-07 21:32:22 +0200

    fix bacon_ls

--- .config/nvim/after/lsp/bacon_ls.lua
+++ .config/nvim/after/lsp/bacon_ls.lua
@@ -2,7 +2,12 @@
 return {
     root_dir = function(bufnr, on_dir)
         local fname = vim.api.nvim_buf_get_name(bufnr)
-        if fname:find(vim.env.HOME .. "/.rustup", 1, true) then return on_dir(nil) end
+        if fname:find(vim.env.HOME .. "/.rustup", 1, true) then return end
         return on_dir(vim.fs.root(bufnr, { ".bacon-locations", "Cargo.toml" }))
     end,
+    settings = {
+        bacon_ls = {
+            cargo = { command = "clippy" },
+        },
+    },
 }
